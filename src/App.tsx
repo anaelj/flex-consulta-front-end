5d55b9577702bf5285d01e0d1010d4a949785733
Anael <anaelj@gmail.com>
2020-08-22 09:13:30 -0400

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import {BrowserRouter as Router} from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import GlobalStyle from './styles/global';
-import  AppProvider  from './hooks';
+import AppProvider from './hooks';
 
 import Routes from './routes';
 
 const App: React.FC = () => (
-    <Router>
-        <AppProvider>
-            <Routes/>
-        </AppProvider>
-        <GlobalStyle />
-    </Router>
+  <Router>
+    <AppProvider>
+      <Routes />
+    </AppProvider>
+    <GlobalStyle />
+  </Router>
 );
 
 export default App;
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -70,11 +70,7 @@ const SignUp: React.FC = () => {
       <Content>
         <AnimationContainer>
           <img src={logoImg} alt="GoBarber" />
-          <Form
-            ref={formRef}
-            initialData={{ name: 'teste' }}
-            onSubmit={handleSubmit}
-          >
+          <Form ref={formRef} onSubmit={handleSubmit}>
             <h1>Faça seu cadastro</h1>
             <Input name="name" icon={FiUser} placeholder="Nome" type="text" />
             <Input

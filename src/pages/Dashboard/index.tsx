c63ff2cc1dfe4402d8df600fcf31a1bc758ad9f7
Anael <anaelj@gmail.com>
2020-08-25 20:39:19 -0400

    correções dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,27 +1,12 @@
-import React, { useState, useCallback, useEffect, useMemo } from 'react';
-import { FiPower, FiClock } from 'react-icons/fi';
-import { isToday, format, parseISO, isAfter } from 'date-fns';
-import ptBR from 'date-fns/locale/pt-BR';
+import React from 'react';
+import { FiPower } from 'react-icons/fi';
 
-import DayPicker, { DayModifiers } from 'react-day-picker';
 import { Link } from 'react-router-dom';
 import logoImg from '../../assets/logo.svg';
 import 'react-day-picker/lib/style.css';
 
-import {
-  Container,
-  Header,
-  Profile,
-  HeaderContent,
-  Content,
-  Schedule,
-  NextAppointment,
-  Section,
-  Appointment,
-  Calendar,
-} from './styles';
+import { Container, Header, Profile, HeaderContent, Content } from './styles';
 import { useAuth } from '../../hooks/auth';
-import api from '../../services/api';
 
 interface MonthAvailabilityItem {
   day: number;
@@ -39,101 +24,7 @@ interface Appointments {
 }
 
 const Dashboard: React.FC = () => {
-  const [selectedDate, setSelectedDate] = useState(new Date());
-  const [selectedMonth, setSelectedMonth] = useState(new Date());
   const { signOut, user } = useAuth();
-  const [appointments, setAppointments] = useState<Appointments[]>([]);
-  const [monthAvailability, setMonthAvailability] = useState<
-    MonthAvailabilityItem[]
-  >([]);
-
-  const handleDateChange = useCallback((day: Date, modifiers: DayModifiers) => {
-    if (modifiers.available) {
-      // havia colocado isso"" && !modifiers.disabled """ para não deixarselecionar dias indisponíveis porem se o dia esta cheio, com varios agendamentos, ele nao deixaa abrir
-      setSelectedDate(day);
-    }
-  }, []);
-
-  const handleMonthChange = useCallback((month: Date) => {
-    setSelectedMonth(month);
-  }, []);
-
-  useEffect(() => {
-    api
-      .get(`/providers/${user.id}/month-availability`, {
-        params: {
-          year: selectedMonth.getFullYear(),
-          month: selectedMonth.getMonth() + 1,
-        },
-      })
-      .then(response => {
-        setMonthAvailability(response.data);
-      });
-  }, [selectedMonth, user.id]);
-
-  useEffect(() => {
-    api
-      .get<Appointments[]>('/appointments/me', {
-        params: {
-          year: selectedDate.getFullYear(),
-          month: selectedDate.getMonth() + 1,
-          day: selectedDate.getDate(),
-        },
-      })
-      .then(response => {
-        const appointmentsFormatted = response.data.map(appointment => {
-          return {
-            ...appointment,
-            hourFormatted: format(parseISO(appointment.date), 'HH:mm'),
-          };
-        });
-
-        setAppointments(appointmentsFormatted);
-        //  console.log(response.data);
-      });
-  });
-
-  const disableDays = useMemo(() => {
-    const dates = monthAvailability
-      .filter(monthDay => monthDay.available === false)
-      .map(monthDay => {
-        const year = selectedMonth.getFullYear();
-        const month = selectedMonth.getMonth();
-
-        return new Date(year, month, monthDay.day);
-      });
-    return dates;
-  }, [selectedMonth, monthAvailability]);
-
-  const selectedDateAsText = useMemo(() => {
-    return format(selectedDate, "'Dia' dd 'de' MMMM", {
-      locale: ptBR,
-    });
-  }, [selectedDate]);
-
-  const selectedweekDay = useMemo(() => {
-    return format(selectedDate, 'cccc', {
-      locale: ptBR,
-    });
-  }, [selectedDate]);
-
-  const morningAppontments = useMemo(() => {
-    return appointments.filter(appointment => {
-      return parseISO(appointment.date).getHours() < 12;
-    });
-  }, [appointments]);
-
-  const afternoonAppontments = useMemo(() => {
-    return appointments.filter(appointment => {
-      return parseISO(appointment.date).getHours() >= 12;
-    });
-  }, [appointments]);
-
-  const nextAppointment = useMemo(() => {
-    return appointments.find(appointment => {
-      return isAfter(parseISO(appointment.date), new Date());
-    });
-  }, [appointments]);
 
   return (
     <Container>

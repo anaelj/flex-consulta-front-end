8660153962dd1434fa33aaf9dce93e9ad1c9f5f7
Anael <anaelj@gmail.com>
2020-09-15 21:15:43 -0400

    correcao ortografica

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 // 'http://192.168.100.8:3333'
 // process.env.REACT_APP_API_URL,
 const api = axios.create({
-  baseURL: 'http://192.168.100.8:3333',
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 export default api;

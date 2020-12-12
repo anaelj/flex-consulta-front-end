2fad513929b43eb8dae5204552d9cf4ffc997b2e
Anael <anaelj@gmail.com>
2020-12-12 12:31:25 -0400

    incluido campo de password no cadastro do usuario

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,6 @@ import axios from 'axios';
 // baseURL:'http://192.168.100.8:3333'
 // baseURL: process.env.REACT_APP_API_URL,
 const api = axios.create({
-  //  baseURL: 'http://192.168.100.8:3333',
   baseURL: process.env.REACT_APP_API_URL,
 });
 
